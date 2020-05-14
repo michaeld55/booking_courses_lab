1727b55a875c28dc3b24e5c207e2ee1bcaa89713
Sam <samhum90@gmail.com>
2020-05-14 13:46:37 +0100

    Added CRUD

--- src/main/java/com/example/codeclan/lab/controllers/BookingController.java
+++ src/main/java/com/example/codeclan/lab/controllers/BookingController.java
@@ -5,10 +5,7 @@ import com.example.codeclan.lab.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,32 @@ public class BookingController {
         if (date != null){
             return new ResponseEntity<>(bookingRepository.findByDate(date), HttpStatus.OK);
         }
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getCourses(@PathVariable Long id){
+        return new ResponseEntity<>(bookingRepository, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Booking> postCourses(@RequestBody Booking booking){
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Booking> putCourses(@RequestBody Booking booking, @PathVariable Long id) {
+        if (booking.getId().longValue() != id){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<List<Booking>> deleteCourses(@PathVariable Long id){
+        bookingRepository.deleteById(id);
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
     }
 }
